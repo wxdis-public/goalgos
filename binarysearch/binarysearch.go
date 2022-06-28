be40f3b7f6ef00637e471bc54e5e307fd2028e17
wxdk <contact@wxdis.com>
2022-06-29 00:33:04 +0100

    Added binary search

---
+++ binarysearch/binarysearch.go
@@ -0,0 +1,21 @@
+package binarysearch
+
+import "golang.org/x/exp/constraints"
+
+// BinarySearch : Check if a value 'target' is present in the supplied list 'vals'.
+// Returns the index of the value if found, and '-1' if the value is not found in
+// the list.
+func BinarySearch[t constraints.Ordered](vals []t, target t) int {
+	l, h := 0, len(vals)-1
+	for l <= h {
+		p := (l + h) >> 1
+		if vals[p] == target {
+			return p
+		} else if vals[p] < target {
+			l = p + 1
+		} else {
+			h = p - 1
+		}
+	}
+	return -1
+}
